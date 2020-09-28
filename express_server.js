49435f5d53aef75a817f4c793ef5f04b41816dd0
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-28 17:35:31 -0600

    updated

--- express_server.js
+++ express_server.js
@@ -23,6 +23,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+})
+
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
