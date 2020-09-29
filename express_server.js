79e2ccd70ec185b255d455fa64105545e4e0c699
Levesque <cj2k@Levesques-MacBook-Pro.local>
2020-09-28 20:47:25 -0600

    updated

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  Math.random().toString(36).substring(0, 6)
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -35,11 +39,14 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars)
 })
 
-
-
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
